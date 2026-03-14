65e069507d84c89a390447a4ac0bb8c76650eeda
Simon Gail <simongail@t-online.de>
2026-03-14 21:45:27 +0100

    update treesitter to main branch

--- nvim/lua/plugins/codeTools/treesitter.lua
+++ nvim/lua/plugins/codeTools/treesitter.lua
@@ -2,33 +2,14 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"bash",
-				"javascript",
-				"json",
-				"xml",
-				"yaml",
-				"regex",
-				"typescript",
-				"html",
-				"css",
-				"json",
-				"python",
-				"php",
-				"rust",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+	opts = {
+		ensure_installed = {
+			"lua", "vim", "vimdoc", "bash",
+			"javascript", "json", "xml", "yaml",
+			"regex", "typescript", "html", "css",
+			"python", "php", "rust", "tmux",
+		},
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
--- nvim/lua/tron/options.lua
+++ nvim/lua/tron/options.lua
@@ -1,4 +1,4 @@
-vim.opt.number = true -- Shows absolute line number on current line
+vim.opt.number = true         -- Shows absolute line number on current line
 vim.opt.relativenumber = true -- Shows relative numbers for other lines
 vim.o.autoindent = true
 vim.o.expandtab = false
@@ -7,15 +7,15 @@ vim.opt.shiftwidth = 4
 vim.o.autoread = true
 vim.o.autowrite = true
 vim.o.autowriteall = true
-vim.opt.updatetime = 250 -- Make this faster (default is 4000)
+vim.opt.updatetime = 250          -- Make this faster (default is 4000)
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-vim.opt.ignorecase = true -- Case insensitive search
-vim.opt.smartcase = true -- ...unless uppercase used
-vim.opt.undofile = true -- Persistent undo history
-vim.opt.signcolumn = "yes" -- Always show sign column (no jumping)
-vim.opt.splitright = true -- Open vertical splits right
-vim.opt.splitbelow = true -- Open horizontal splits below
-vim.opt.termguicolors = true -- True color support
+vim.opt.ignorecase = true         -- Case insensitive search
+vim.opt.smartcase = true          -- ...unless uppercase used
+vim.opt.undofile = true           -- Persistent undo history
+vim.opt.signcolumn = "yes"        -- Always show sign column (no jumping)
+vim.opt.splitright = true         -- Open vertical splits right
+vim.opt.splitbelow = true         -- Open horizontal splits below
+vim.opt.termguicolors = true      -- True color support
 -- Scrolloffset
 vim.opt.scrolloff = 4
 vim.opt.sidescrolloff = 6
@@ -26,4 +26,5 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.foldopen:remove("hor") -- Don't open folds when moving horizontally
-vim.o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:"
+-- use Neovim nightly branch
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
