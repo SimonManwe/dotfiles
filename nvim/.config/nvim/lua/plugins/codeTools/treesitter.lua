d603ba173a78f28ec36be8efccbb44d39f3b943f
Simon Gail <simongail@t-online.de>
2026-03-16 07:51:44 +0100

    make nvim config work with treesitter main branch

--- nvim/.config/nvim/lua/plugins/codeTools/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/codeTools/treesitter.lua
@@ -1,15 +1,43 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		ensure_installed = {
-			"lua", "vim", "vimdoc", "bash",
-			"javascript", "json", "xml", "yaml",
-			"regex", "typescript", "html", "css",
-			"python", "php", "rust", "tmux",
-		},
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+	config = function()
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+		local ts = require("nvim-treesitter")
+
+		ts.install({
+			"lua",
+			"vim",
+			"vimdoc",
+			"bash",
+			"javascript",
+			"json",
+			"xml",
+			"yaml",
+			"regex",
+			"typescript",
+			"html",
+			"css",
+			"python",
+			"php",
+			"rust",
+			"tmux",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
 }
