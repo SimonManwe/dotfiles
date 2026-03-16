6a3aa04145010da93f327b5d6e63504affc177cc
Simon Gail <simongail@t-online.de>
2026-03-16 09:00:27 +0100

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 PACKAGES=(
-    nvim
-    kitty
-    zsh
-    tmux
+	nvim
+	kitty
+	zsh
+	tmux
 	yazi
 )
 
 echo "Setting up dotfiles..."
 
-if ! command -v stow &> /dev/null; then
-    echo "stow not found, please install gnu stow"
+if ! command -v stow &>/dev/null; then
+	echo "stow not found, please install gnu stow"
 fi
 
 for pkg in "${PACKAGES[@]}"; do
-    echo "Stowing $pkg..."
-    stow --target="$HOME" "$pkg"
-    if [ $? -eq 0 ]; then
-        echo "✓ $pkg symlink creation successfull"
-    else
-        echo "✗ Error: $pkg – is there an existing config already?"
-    fi
+	echo "Stowing $pkg..."
+	stow --target="$HOME" "$pkg"
+	if [ $? -eq 0 ]; then
+		echo "✓ $pkg symlink creation successfull"
+	else
+		echo "✗ Error: $pkg – is there an existing config already?"
+	fi
 done
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "Installing TPM for tmux..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo "✓ TPM successfully installed"
+	echo "Installing TPM for tmux..."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	echo "✓ TPM successfully installed"
 else
-    echo "✓ TPM already installed"
+	echo "✓ TPM already installed"
 fi
 
 if [ ! -d "$HOME/.tmux/plugins/catppuccin" ]; then
-    echo "Installing catppuccin tmux..."
-    git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
-    echo "✓ Catppuccin installed successfully"
+	echo "Installing catppuccin tmux..."
+	git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
+	echo "✓ Catppuccin installed successfully"
 else
-    echo "✓ Catppuccin  installed"
+	echo "✓ Catppuccin already installed"
 fi
 
 echo "Dotfiles setup complete! Start tmux and press prefix + I to install plugins"
