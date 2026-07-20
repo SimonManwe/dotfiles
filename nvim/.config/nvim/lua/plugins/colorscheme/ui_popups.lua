8a12e6f96b5a982d4cee2f9276598690b04c1beb
SimonManwe <simongail@t-online.de>
2026-07-20 21:10:31 +0200

    add ui popups using noice.nvim

---
+++ nvim/.config/nvim/lua/plugins/colorscheme/ui_popups.lua
@@ -0,0 +1,51 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		lsp = {
+			-- override markdown rendering so that cmp and other plugins use Treesitter
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		presets = {
+			bottom_search = true, -- keep classic bottom cmdline for live search
+			command_palette = true, -- position cmdline + popupmenu together (floating)
+			long_message_to_split = true, -- long messages go to a split, not a big popup
+			inc_rename = false,  -- only relevant for inc-rename.nvim
+			lsp_doc_border = false, -- no border on hover/signature popups
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "yank",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "put",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "more lines",
+				},
+				opts = { skip = true },
+			},
+		},
+	},
+}
