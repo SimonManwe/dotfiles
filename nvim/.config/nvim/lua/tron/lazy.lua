39a3523f0eff326b30dfd971584f44951d3252bb
SimonManwe <simongail@t-online.de>
2026-07-20 21:40:55 +0200

    add git tools to lazy tracked folders

--- nvim/.config/nvim/lua/tron/lazy.lua
+++ nvim/.config/nvim/lua/tron/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -34,6 +34,7 @@ require("lazy").setup({
 		{ import = "plugins.php" },
 		{ import = "plugins.experimental" },
 		{ import = "plugins.codeTools" },
+		{ import = "plugins.git_tools" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
