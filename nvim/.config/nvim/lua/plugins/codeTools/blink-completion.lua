008c39b536f60a9c813477c573427cc9dccab7ef
Simon Gail <simongail@t-online.de>
2026-08-10 16:15:47 +0200

    add blink cmp

---
+++ nvim/.config/nvim/lua/plugins/codeTools/blink-completion.lua
@@ -0,0 +1,68 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = {
+		"saghen/blink.lib",
+		-- optional: provides snippets for the snippet source
+		"rafamadriz/friendly-snippets",
+	},
+	build = function()
+		-- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
+		-- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+		require("blink.cmp").build():pwait()
+	end,
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+
+		keymap = {
+			preset = "default",
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+		},
+
+		completion = {
+			keyword = { range = "prefix" },
+			menu = {
+				border = "rounded",
+				draw = {
+					columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
+				},
+			},
+			documentation = {
+				auto_show = false,
+				window = { border = "rounded" },
+			},
+			ghost_text = { enabled = false },
+		},
+
+		sources = {
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			per_filetype = {
+				lua = { inherit_defaults = true, "lazydev" },
+			},
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
+				buffer = {
+					min_keyword_length = 2,
+					max_items = 50,
+				},
+			},
+		},
+		cmdline = {
+			enabled = true,
+		},
+
+		fuzzy = { implementation = "rust" },
+	},
+}
