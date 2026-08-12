76e143027ec72390dd5519ec7aeebd5aabee3b62
Simon Gail <simongail@t-online.de>
2026-08-12 10:07:22 +0200

    fully configure snacks.nvim

--- nvim/.config/nvim/lua/plugins/colorscheme/snacks.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme/snacks.lua
@@ -2,8 +2,169 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 	---@type snacks.Config
 	opts = {
 		input = { enabled = true },
+		indent = { enabled = true, animate = { enabled = false } },
+		lazygit = { enabled = true },
+		picker = {
+			enabled = true,
+			ui_select = true,
+			layout = {
+				preset = "vertical",
+				layout = {
+					box = "vertical",
+					width = 0.9,
+					min_width = 130,
+					height = 0.9,
+					{
+						box = "vertical",
+						border = true,
+						title = "{title} {live} {flags}",
+						{ win = "input", height = 1, border = "bottom" },
+						{ win = "list", border = "none" },
+					},
+					{ win = "preview", title = "{preview}", border = true, height = 0.65 },
+				},
+			},
+		},
+	},
+	keys = {
+		-- Top Pickers & Explorer
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fF",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>sn",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
+		-- git
+		{
+			"<leader>gb",
+			function()
+				Snacks.picker.git_branches()
+			end,
+			desc = "Git Branches",
+		},
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_stash()
+			end,
+			desc = "Git Stash",
+		},
+		{
+			"<leader>gd",
+			function()
+				Snacks.picker.git_diff()
+			end,
+			desc = "Git Diff (Hunks)",
+		},
+		-- LSP
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations()
+			end,
+			desc = "Goto Declaration",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "References",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "Goto Implementation",
+		},
+		{
+			"<leader>gT",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "Goto [T]ype Definition",
+		},
+		{
+			"<leader>ls",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
+		},
+		{
+			"<leader>lS",
+			function()
+				Snacks.picker.lsp_workspace_symbols()
+			end,
+			desc = "LSP Workspace Symbols",
+		},
+		-- Lazygit
+		---@param opts? snacks.lazygit.Config
+		{
+			"lg",
+			function()
+				Snacks.lazygit.open()
+			end,
+			desc = "Lazygit",
+		},
+		-- Buffer
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
 	},
 }
